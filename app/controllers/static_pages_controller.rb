1637cd41b6ec687775d6c03687a7a87a84ffd52a
Sabrina <sabrina.leskovsek@gmail.com>
2017-10-04 10:01:24 +0200

    5.3 emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,15 +10,9 @@ class StaticPagesController < ApplicationController
   end
 end
 
-ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Thanks 
-	for your message", body: 'I received your message and will shortly get back to you.').deliver_now
-
 def thank_you
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
-      to: 'sabrina.leskovsek@gmail.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
+  UserMailer.contact_form(@email, @name, @message).deliver_now
 end
