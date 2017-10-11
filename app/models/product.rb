5e8c4eb4f4a4372b1db080c42202b87e3d66dd2a
Sabrina <sabrina.leskovsek@gmail.com>
2017-10-11 06:11:58 +0200

    5.7 fixed product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
 	has_many :comments
-	dependent :destroy
 	def self.search(search_term)
   	if Rails.env.development?
   		Product.where("name LIKE ?", "%#{search_term}%")
